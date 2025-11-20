0469b1a49c3e493b2bad109207b512e669afd61d
KALAIYARASU20 <kalaiyarasuelangovan@gmail.com>
2025-11-20 14:47:27 +0530

    first commit

---
+++ kalai.py
@@ -0,0 +1,12 @@
+student={"Name":"KALAIYARASU",
+         "Age":18,
+         "Roll number":777,
+         "Department":"Artificial intelligence and data science",
+         "Year":2,
+         "Semester":3}
+print(student["Name"])
+print(student["Age"])
+print(student["Roll number"])
+print(student["Department"])
+print(student["Year"])
+print(student["Semester"])
